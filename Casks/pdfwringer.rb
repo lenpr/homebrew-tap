5d20aa997118be25429e55379aaf0376569263dd
Lukas N.P. Egger <egger.lukas@gmx.net>
2026-05-12 00:10:01 +0200

    Update PDFwringer SHA256 for v0.1.13

--- Casks/pdfwringer.rb
+++ Casks/pdfwringer.rb
@@ -1,6 +1,6 @@
 cask "pdfwringer" do
   version "0.1.13"
-  sha256 "PLACEHOLDER"
+  sha256 "521d8f424f005cbee122f6e913a4c0d4f7ecc7aa2a9aa138015fc916b1895b02"
 
   url "https://github.com/lenpr/PDFwringer/releases/download/v#{version}/PDFwringer.dmg"
   name "PDFwringer"
